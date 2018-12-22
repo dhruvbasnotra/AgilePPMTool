37e38930504aacaec9460aecc8b3257f2d878483
dhruv basnotra <dhruvbasnotra@dhruvs-mbp.attlocal.net>
2018-12-22 15:46:12 -0600

    Project branch7

--- ppmtool/src/main/java/com/dhruv/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/com/dhruv/ppmtool/repositories/ProjectRepository.java
@@ -11,5 +11,6 @@ public interface ProjectRepository extends CrudRepository<Project,Long> {
 
 Project findByProjectIdentifier(String projectId);
 
-
+    @Override
+    Iterable<Project> findAll();
 }
